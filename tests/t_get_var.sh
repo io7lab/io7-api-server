d3c08815d07d885f5afd21fd81b5423c9227a697
Yoonseok Hur <yoonseok@gmail.com>
2025-06-24 17:36:16 +0900

    test case

--- tests/t_get_var.sh
+++ tests/t_get_var.sh
@@ -5,11 +5,12 @@ echo $pw
 token=$(curl -X POST 'http://localhost:2009/users/login' -H 'Content-Type: application/json' -d "{ \"email\": \"io7@io7lab.com\", \"password\": \"$pw\" }"|jq '.access_token'|tr -d '"') 2>/dev/null
 
 curl -X 'GET' \
-  'http://localhost:2009/config/influxdb_token' \
+  'http://localhost:2009/config/' \
   -H 'accept: application/json' \
-  -H "Authorization: Bearer $token" 
+  -H "Authorization: Bearer $token"  | jq
+
 exit
 curl -X 'GET' \
-  'http://localhost:2009/config/' \
+  'http://localhost:2009/config/influxdb_token' \
   -H 'accept: application/json' \
   -H "Authorization: Bearer $token" 
